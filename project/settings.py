efd2bd32973ef97c3944aed4c650c0636ad40ef3
Mark Konakov <konakovmark@yandex.ru>
2021-09-30 14:37:40 +0300

    Set db settings from .env

--- project/settings.py
+++ project/settings.py
@@ -5,15 +5,20 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 SECRET_KEY = os.getenv('SECRET_KEY')
+DB_ENGINE = os.getenv('DB_ENGINE')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
+        'ENGINE': DB_ENGINE,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
+        'NAME': DB_NAME,
+        'USER': DB_USER,
         'PASSWORD': DB_PASS,
     }
 }
